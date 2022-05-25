7f3d78cb7dabe186c4dc2e6f52339547a3b8f531
fufeisi <55070756+fufeisi@users.noreply.github.com>
2022-05-25 11:19:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ parser.add_argument('--arch', '-a', metavar='ARCH', default='vgg19',
                     ' (default: vgg19)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=300, type=int, metavar='N',
+parser.add_argument('--epochs', default=600, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
